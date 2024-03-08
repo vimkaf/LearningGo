e5f36ce998b022820bd8ce39616f5a156b2d9302
ThaOracle <vimkaf@gmail.com>
2024-03-09 00:01:29 +0100

    Learnt how to create a module and call it from another module

---
+++ 2/whatistime/readme.md
@@ -0,0 +1,3 @@
+# This lesson talks about using the module I created previous in this module
+
+[Documentation](https://go.dev/doc/tutorial/call-module-code)
---
+++ 2/whatistime/time.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vimkaf/ngtime"
+)
+
+func main() {
+
+	fmt.Println("The time in Nigeria is", ngtime.getTimeInNigeria())
+}
